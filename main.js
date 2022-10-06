a62f49f7213fa53e48dba8c2935abbf88868ff2d
Igor Berroteran <berroteran.ig@gmail.com>
2022-10-06 20:47:21 +0200

    Estilos

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
+
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 
@@ -14,21 +15,21 @@ menuCarritoIcon.addEventListener('click', toogleCarritoDesktop);
 
 function toogleDesktopMenu(){
    mobileMenu.classList.add('inactive');
-   aside.classList.add('inactive');
+   shoppingCartContainer.classList.add('inactive');
    desktopMenu.classList.toggle('inactive');
 }
 
 
 function toogleMobileMenu(){
     desktopMenu.classList.add('inactive');
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
 }
 
 function  toogleCarritoDesktop(){
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
